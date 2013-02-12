d6a87cf97596aaa7f2d880f5865f9810a6a5a1c0
Stephen Demjanenko <sdemjanenko@gmail.com>
2013-02-11 20:58:48 -0800

    Add modals, load forms from their templates

--- roulette.js
+++ roulette.js
@@ -1,3 +1,16 @@
+$(init);
+
 function init() {
-  $('h1').text('Welcome: javascript works');
+  $('#createMeeting').click(createMeeting);
+  $('#joinMeeting').click(joinMeeting);
+}
+
+function createMeeting() {
+  console.log("creating meeting");
+  $("#create").submit();
+}
+
+function joinMeeting() {
+  console.log("joining meeting");
+  $("#join").submit();
 }
