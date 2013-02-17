6171560605894f932ade6357b106173ddc52f085
Stephen Demjanenko <sdemjanenko@gmail.com>
2013-02-17 13:45:49 -0800

    Break our DB queries out into database.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
-var redis = require('redis');
-var db = redis.createClient();
+var Meeting = require('./routes/database').Meeting;
 var app = express();
 var PORT = 3000;
 
@@ -8,11 +7,6 @@ app.set('title', 'Protonight-Roulette');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
-
-
-// setup redis helpers
-//redis.debug_mode = true;
-//db.set('', '');
 
 
 // Routes stuff
@@ -23,29 +17,8 @@ app.get('/', function(req, res) {
 // get the list of meetings
 app.get('/meetings', function(req, res) {
   //var req_ip = req.ip; // filter the meetings by IP so that users are connected to event wifi
-  fetchMeetings(function(data) { res.send(data); });
+  Meeting.findAll(function(data) { res.send(data); });
 });
-
-function fetchMeetings(callback) {
-  var data = [];
-  db.zremrangebyscore('meetings', 0, (new Date()).getTime()); // clear out old keys from the meetings set
-  db.zrevrangebyscore([ 'meetings', "+inf", "-inf" ], function(err, res) {
-    if (err) throw err;
-    res = res || [];
-    if (res.length === 0)
-      callback([]);
-    else {
-      res.forEach(function(key) {
-        db.hgetall(key, function(err, val) {
-          if (err) throw err;
-          data.push(val);
-          if (data.length === res.length)
-            callback(data);
-        });
-      });
-    }
-  });
-}
 
 // add a new meeting
 app.post('/meetings', function(req, res) {
@@ -56,33 +29,17 @@ app.post('/meetings', function(req, res) {
       count: 0
   };
 
-  db.incr('next_id/meetings', function(err, id) {
-    var key = "meetings/" + id;
-    console.log('key', key, data);
-    db.hmset(key, data, function(err) {
-      db.zadd("meetings", (new Date()).getTime() + 10000, key);
-      db.expire(key, 10); // only let the key live for 10 seconds
-    });
+  Meeting.create(data, function(meeting) {
+    res.send(meeting);
   });
-
-  res.send(data);
 });
 
 // show details for a particular meeting
 app.get('/meeting/:id', function(req, res) {
-  var id = req.params.id,
-      data = {};
-
-  if (id === 1) {
-    data = {
-      id: 1,
-      title: "test1",
-      skills: ['ideas', 'developer', 'designer'],
-      members: ['Steve', 'Ben', 'Brian']
-    };
-  }
-
-  res.send(data);
+  var id = req.params.id;
+  Meeting.find(id, function(meeting) {
+    res.send(meeting);
+  });
 });
 
 app.listen(PORT);
---
+++ routes/database.js
@@ -0,0 +1,52 @@
+var redis = require('redis');
+var db = redis.createClient();
+
+// setup redis helpers
+//redis.debug_mode = true;
+//db.set('', '');
+
+var dbExpire = 10; // seconds
+var Meeting = {};
+
+Meeting.findAll = function(callback) {
+  var data = [];
+  db.zremrangebyscore('meetings', 0, (new Date()).getTime()/1000); // clear out old keys from the meetings set
+  db.zrevrangebyscore([ 'meetings', "+inf", "-inf" ], function(err, res) {
+    if (err) throw err;
+    res = res || [];
+    if (res.length === 0)
+      callback([]);
+    else {
+      res.forEach(function(key) {
+        db.hgetall(key, function(err, val) {
+          if (err) throw err;
+          data.push(val);
+          if (data.length === res.length)
+            callback(data);
+        });
+      });
+    }
+  });
+};
+
+Meeting.find = function(id, callback) {
+  db.hgetall("meetings/" + id, function(err, val) {
+    if (err) throw err;
+    callback(val);
+  });
+};
+
+Meeting.create = function(attributes, callback) {
+  db.incr('next_id/meetings', function(err, id) {
+    var key = "meetings/" + id;
+    attributes.id = id;
+    db.hmset(key, attributes, function(err) {
+      if (err) throw err;
+      db.zadd("meetings", (new Date()).getTime()/1000 + dbExpire, key);
+      db.expire(key, dbExpire); // only let the key live for 10 seconds
+      callback(attributes);
+    });
+  });
+};
+
+exports.Meeting = Meeting;
