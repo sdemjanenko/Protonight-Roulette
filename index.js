f9c0af1565f970dcd778d386b9d7434111c6f279
Stephen Demjanenko <sdemjanenko@gmail.com>
2013-02-16 23:38:27 -0800

    Use backbone & backbone stickit to handle the meeting list

--- index.js
+++ index.js
@@ -15,12 +15,39 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-app.get('/user/:id', function(req, res) {
-  res.send('user ' + req.params.id);
+app.get('/meetings', function(req, res) {
+  var data = [ {
+      id: 1,
+      title: "test1",
+      skills: ['ideas', 'developer', 'designer'],
+      count: 3
+    }, {
+      id: 2,
+      title: "test2",
+      skills: ['soccer', 'football'],
+      count: 1
+  } ];
+  res.send(data);
 });
 
 app.get('/meeting/:id', function(req, res) {
-  res.send('meeting ' + req.params.id);
+  var id = req.params.id,
+      data = {};
+
+  if (id === 1) {
+    data = {
+      id: 1,
+      title: "test1",
+      skills: ['ideas', 'developer', 'designer'],
+      members: ['Steve', 'Ben', 'Brian']
+    };
+  }
+
+  res.send(data);
+});
+
+app.get('/user/:id', function(req, res) {
+  res.send('user ' + req.params.id);
 });
 
 app.post('/meeting/:meeting_id/user/:user_id', function(req, res) {
--- public/assets/javascripts/roulette.js
+++ public/assets/javascripts/roulette.js
@@ -1,9 +1,77 @@
 $(init);
 
+var MeetingModel = Backbone.Model.extend({
+  defaults: {
+    count: 0
+  }
+});
+
+var MeetingCollection = Backbone.Collection.extend({
+  model: MeetingModel
+});
+
+var MeetingRowView = Backbone.View.extend({
+  skeleton: $("#meetingRowSkeleton"),
+  tagName: "li",
+  className: "meeting-row",
+  events: {
+    "click": "open"
+  },
+  bindings: {
+    ".title": "title",
+    ".skills": "skills",
+    ".count": "count"
+  },
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+    this.render();
+  },
+  render: function() {
+    this.$el = cloneSkeleton(this.skeleton);
+    this.stickit();
+  },
+  open: function() {
+    console.log("opening", this);
+  }
+});
+
+var MeetingListView = Backbone.View.extend({
+  tagName: "ul",
+  className: "meeting-list",
+  initialize: function() {
+    this.listenTo(this.collection, "reset", this.render);
+  },
+  render: function() {
+    var _this = this;
+    _.each(this.collection.models, function(model) {
+      _this.add(model);
+    });
+    return this;
+  },
+  add: function(model) {
+    var view = new MeetingRowView({ model: model });
+    this.$el.append(view.$el);
+  }
+});
+
+function cloneSkeleton(el) {
+  var clone = el.clone(true);
+  clone.removeAttr('id');
+  return clone;
+}
+
+
+var meetings = new MeetingCollection();
+var meetings_view = new MeetingListView({collection: meetings, el: "#list" });
 function init() {
   $('#createButton').click(createMeeting);
   $('#joinButton').click(joinMeeting);
+
+  $.get("/meetings", function(data) {
+    meetings.reset(data);
+  });
 }
+
 
 function createMeeting() {
   //$("#create").submit();
@@ -28,3 +96,5 @@ function joinMeeting() {
   console.log("joining meeting");
   //$("#join").submit();
 }
+
+
